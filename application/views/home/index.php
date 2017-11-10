b27b20507349db9f0a4076527e8b17ee9fa62e1d
toxlexz <heruressha99@gmail.com>
2017-11-10 14:17:32 +0700

    template

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -98,30 +98,9 @@
                 <form action="#" id="mobile-nav" class="visible-phone">
                     <div class="mobile-nav-select">
                         <select onchange="window.open(this.options[this.selectedIndex].value,'_top')">
-                    <option value="">Navigate...</option>
-                    <option value="index.htm">Home</option>
-                        <option value="index.htm">- Full Page</option>
-                        <option value="index-gallery.htm">- Gallery Only</option>
-                        <option value="index-slider.htm">- Slider Only</option>
-                    <option value="features.htm">Features</option>
-                    <option value="page-full-width.htm">Pages</option>
-                        <option value="page-full-width.htm">- Full Width</option>
-                        <option value="page-right-sidebar.htm">- Right Sidebar</option>
-                        <option value="page-left-sidebar.htm">- Left Sidebar</option>
-                        <option value="page-double-sidebar.htm">- Double Sidebar</option>
-                    <option value="gallery-4col.htm">Gallery</option>
-                        <option value="gallery-3col.htm">- 3 Column</option>
-                        <option value="gallery-4col.htm">- 4 Column</option>
-                        <option value="gallery-6col.htm">- 6 Column</option>
-                        <option value="gallery-4col-circle.htm">- Gallery 4 Col Round</option>
-                        <option value="gallery-single.htm">- Gallery Single</option>
-                    <option value="blog-style1.htm">Blog</option>
-                        <option value="blog-style1.htm">- Blog Style 1</option>
-                        <option value="blog-style2.htm">- Blog Style 2</option>
-                        <option value="blog-style3.htm">- Blog Style 3</option>
-                        <option value="blog-style4.htm">- Blog Style 4</option>
-                        <option value="blog-single.htm">- Blog Single</option>
-                    <option value="page-contact.htm">Contact</option>
+                    <option value="">Navigasi</option>
+                    <option value="<?php echo site_url('user_authentication/login');?>">Login</option>
+                       >
                 </select>
                     </div>
                 </form>
@@ -198,117 +177,11 @@
                     </ul>
                 </div>
             </div>
-
         </div>
-        <!-- End Gallery Row -->
-
-        <div class="row">
-            <!-- Begin Bottom Section -->
-
-            <!-- Blog Preview
-        ================================================== -->
-            <div class="span6">
-
-                <h5 class="title-bg">From the Blog
-                    <small>All the latest news</small>
-                    <button id="btn-blog-next" class="btn btn-inverse btn-mini" type="button">&raquo;</button>
-                    <button id="btn-blog-prev" class="btn btn-inverse btn-mini" type="button">&laquo;</button>
-                </h5>
-
-                <div id="blogCarousel" class="carousel slide ">
-
-                    <!-- Carousel items -->
-                    <div class="carousel-inner">
-
-                        <!-- Blog Item 1 -->
-                        <div class="active item">
-                            <a href="#"><img src="<?php echo site_url('template/img/gallery/blog-med-img-1.jpg');?>" alt="" class="align-left blog-thumb-preview" /></a>
-                            <div class="post-info clearfix">
-                                
-                                <ul class="blog-details-preview">
-                                    <li><i class="icon-calendar"></i><strong>Posted on:</strong> Sept 4, 2015
-                                        <li>
-                                            <li><i class="icon-user"></i><strong>Posted by:</strong> <a href="#" title="Link">Admin</a>
-                                                <li>
-                                                    <li><i class="icon-comment"></i><strong>Comments:</strong> <a href="#" title="Link">12</a>
-                                                        <li>
-                                                            <li><i class="icon-tags"></i> <a href="#">photoshop</a>, <a href="#">tutorials</a>, <a href="#">illustration</a>
-                                </ul>
-                            </div>
-                            
-                        </div>
-
-                        <!-- Blog Item 2 -->
-                        <div class="item">
-                            <a href="#"><img src="<?php echo site_url('template/img/gallery/blog-med-img-1.jpg');?>" alt="" class="align-left blog-thumb-preview" /></a>
-                            <div class="post-info clearfix">
-                                <h4><a href="blog-single.htm">A great artist is always before his time</a></h4>
-                                <ul class="blog-details-preview">
-                                    <li><i class="icon-calendar"></i><strong>Posted on:</strong> Sept 4, 2015
-                                        <li>
-                                            <li><i class="icon-user"></i><strong>Posted by:</strong> <a href="#" title="Link">Admin</a>
-                                                <li>
-                                                    <li><i class="icon-comment"></i><strong>Comments:</strong> <a href="#" title="Link">12</a>
-                                                        <li>
-                                                            <li><i class="icon-tags"></i> <a href="#">photoshop</a>, <a href="#">tutorials</a>, <a href="#">illustration</a>
-                                </ul>
-                            </div>
-                            <p class="blog-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum felis fermentum ipsum molestie sed porttitor ligula rutrum. Vestibulum lectus tellus, aliquet et iaculis sed, volutpat vel erat. <a href="#">Read more</a>
-                                <p>
-                        </div>
-
-                        <!-- Blog Item 3 -->
-                        <div class="item">
-                            <a href="#"><img src="<?php echo site_url('template/img/gallery/blog-med-img-1.jpg');?>" alt="" class="align-left blog-thumb-preview" /></a>
-                            <div class="post-info clearfix">
-                                <h4><a href="blog-single.htm">Is art everything to anybody?</a></h4>
-                                <ul class="blog-details-preview">
-                                    <li><i class="icon-calendar"></i><strong>Posted on:</strong> Sept 4, 2015
-                                        <li>
-                                            <li><i class="icon-user"></i><strong>Posted by:</strong> <a href="#" title="Link">Admin</a>
-                                                <li>
-                                                    <li><i class="icon-comment"></i><strong>Comments:</strong> <a href="#" title="Link">12</a>
-                                                        <li>
-                                                            <li><i class="icon-tags"></i> <a href="#">photoshop</a>, <a href="#">tutorials</a>, <a href="#">illustration</a>
-                                </ul>
-                            </div>
-                            <p class="blog-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum felis fermentum ipsum molestie sed porttitor ligula rutrum. Vestibulum lectus tellus, aliquet et iaculis sed, volutpat vel erat. <a href="#">Read more</a>
-                                <p>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <!-- Client Area
-        ================================================== -->
-            <div class="span6">
-
-                <h5 class="title-bg">Recent Clients
-                    <small>That love us</small>
-                    <button id="btn-client-next" class="btn btn-inverse btn-mini" type="button">&raquo;</button>
-                    <button id="btn-client-prev" class="btn btn-inverse btn-mini" type="button">&laquo;</button>
-                </h5>
-
-                <!-- Client Testimonial Slider-->
-                <div id="clientCarousel" class="carousel slide no-margin">
-                    <!-- Carousel items -->
-                    <div class="carousel-inner">
-
-                        
-                    </div>
-                </div>
-
-                <!-- Client Logo Thumbs-->
-               
-
-            </div>
-
-        </div>
-        <!-- End Bottom Section -->
-
+      </div>
     </div>
-    <!-- End Container -->
+   
+
 
     <!-- Footer Area
         ================================================== -->
@@ -319,7 +192,7 @@
             <div class="row footer-row">
                 <div class="span3 footer-col">
                     <h5>About Us</h5>
-                    <img src="template/img/piccolo-footer-logo.png" alt="Piccolo" /><br /><br />
+                    <img src="<?php echo site_url('template/img/piccolo-footer-logo.png');?>" alt="Piccolo" /><br /><br />
                     <address>
                         <strong>......</strong><br />
                         ......................<br />
@@ -343,20 +216,18 @@
                         </li>
                     </ul>
                 </div>
-              
-               
-               
+             
 
             <div class="row">
                 <!-- Begin Sub Footer -->
                 <div class="span12 footer-col footer-sub">
                     <div class="row no-margin">
-                        <div class="span6"><span class="left">Copyright 2012 Piccolo Theme. All rights reserved.</span></div>
+                        <div class="span6"><span class="left">????????????????????????????????????</span></div>
                         <div class="span6">
                             <span class="right">
-                            <a href="#">Home</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-                            <a href="#">Features</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">
-                                Gallery</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">Blog</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">Contact</a>
+                            <a href="#">????????</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                            <a href="#">????????</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">
+                                ???????</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">?????????</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="#">?????????</a>
                             </span>
                         </div>
                     </div>
