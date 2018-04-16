e20c0303da62966d8b392cf8f58a7a43f75fb3b3
rayzalzero <rayzalzero@gmail.com>
2018-04-16 19:00:03 +0700

    change

--- application/views/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($this->session->userdata['logged_in'])) {
-    header("location:". site_url('/dashboard')."");
-}else {
-    print_r("iyo");
-    //header("location:". site_url('/')."");
-}
-?>
